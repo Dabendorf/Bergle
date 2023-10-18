103f90c744c2c8b44e7e6fb0673dbb8e809d7889
Lukas Schramm <lukas@dorpapst.de>
2023-10-18 15:47:49 +0200

    English (norsk) language per default

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,4 @@
 import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
 const resources = {
@@ -29,7 +28,6 @@ const resources = {
 
 i18n
   .use(initReactI18next)
-  .use(LanguageDetector)
   .init({
     resources,
     interpolation: {
