83140ae5cc13f204d04628d5cfc15a219660a490
Alexander Iversen <alexiver96@hotmail.com>
2023-10-28 16:28:19 +0200

    uuuh

--- src/components/panels/Panel.tsx
+++ src/components/panels/Panel.tsx
@@ -23,7 +23,7 @@ export function Panel({ title, isOpen, close, children }: PanelProps) {
       shouldCloseOnEsc={true}
       //ariaHideApp
     >
-      <div className="w-96 h-full overflow-auto mt-10 pb-10 bg-slate-100 dark:bg-slate-800 rounded-xl">
+      <div className="w-96 h-full overflow-auto mt-10 pb-10 slate-100 bg-slate-100 dark:bg-slate-800 rounded-xl">
         <header className="flex justify-between items-center px-4 py-2 border-b border-gray-300" style={{ color: "slate-100" }}>
           <h2 className="text-lg font-medium slate-100">{title}</h2>
           <button
