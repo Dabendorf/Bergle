94bbd05be59f78dbc68a20f4920e285b71ae36d2
AalyTokombaev <alexiver96@hotmail.com>
2024-01-21 23:04:26 +0100

    updated react deps and tried reagraph

--- src/components/panels/Help.tsx
+++ src/components/panels/Help.tsx
@@ -1,5 +1,24 @@
 import React from "react";
 import { Panel } from "./Panel";
+import { GraphCanvas, GraphNode, GraphEdge } from "reagraph";
+
+interface Node {
+  id: number;
+  label: string;
+}
+
+interface Edge {
+  from: number;
+  to: number;
+  id: number;
+}
+
+const nodes: GraphNode[] = [
+  { id: "1", label: "1" },
+  { id: "2", label: "2" },
+];
+
+const edges: GraphEdge[] = [{ source: "1", target: "2", id: "1" }];
 
 interface HelpProps {
   isOpen: boolean;
@@ -12,6 +31,7 @@ export default function Help({ isOpen, close }: HelpProps) {
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-2xl font-bold">Hjelp</h1>
         <p className="text-lg">Hjelp kommer</p>
+        <GraphCanvas nodes={nodes} edges={edges} />
       </div>
     </Panel>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,18 @@ import "./index.css";
 import "./i18n";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import ts from "typescript";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const root = document.getElementById("root");
+
+if (root !== null) {
+  // @ts-ignore
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
