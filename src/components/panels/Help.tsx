69bc45f7ffe3286500e2c446f07de93cd4119e09
AalyTokombaev <alexiver96@hotmail.com>
2024-01-23 18:43:17 +0100

    extended GraphNode interface instead of makind a new list

--- src/components/panels/Help.tsx
+++ src/components/panels/Help.tsx
@@ -7,18 +7,19 @@ import {
   NodePositionArgs,
   InternalGraphPosition,
 } from "reagraph";
-import { bergenNodes } from "../../domain/neighborhoods";
 
-const nodes: GraphNode[] = bergenNodes.map((bgnode) => ({
-  id: bgnode.id,
-  label: bgnode.name,
-}));
+import { mapNodes } from "../../domain/neighborhoods";
+
 const edges: GraphEdge[] = [];
 
-function getNodeCoordinates(nodeId: string): InternalGraphPosition {
-  const node = bergenNodes.find((n) => n.id === nodeId);
+function getNodePosition(
+  id: string,
+  nodes: NodePositionArgs
+): InternalGraphPosition {
+  const node = mapNodes.find((n) => n.id === id);
   if (node) {
     return {
+      // TODO: Formulate a different basis and apply to coordinates.
       x: node.lon * 1500,
       y: node.lat * 1500,
       z: 1,
@@ -27,10 +28,10 @@ function getNodeCoordinates(nodeId: string): InternalGraphPosition {
   return {} as InternalGraphPosition;
 }
 
-for (const bgnode of bergenNodes) {
-  for (const nbId of bgnode.neighborsIds) {
+for (const mapNode of mapNodes) {
+  for (const nbId of mapNode.neighborsIds) {
     const src = nbId;
-    const target = bgnode.id;
+    const target = mapNode.id;
     const id = `${src}-${target}`;
     const revId = `${target}-${src}`;
 
@@ -65,19 +66,11 @@ export default function Help({ isOpen, close }: HelpProps) {
         <p className="text-lg">Hjelp kommer</p>
         <div style={{ width: "500px", height: "500px" }}>
           <GraphCanvas
-            nodes={nodes}
+            nodes={mapNodes}
             edges={edges}
             onCanvasClick={() => close()}
             layoutType="custom"
-            layoutOverrides={{
-              getNodePosition: (
-                id: string,
-                args: NodePositionArgs
-              ): InternalGraphPosition => {
-                const coords = getNodeCoordinates(id);
-                return coords;
-              },
-            }}
+            layoutOverrides={{ getNodePosition }}
           />
         </div>
       </div>
--- src/domain/neighborhoods.ts
+++ src/domain/neighborhoods.ts
@@ -1,60 +1,67 @@
-interface BergenNode {
+import { GraphNode } from "reagraph";
+
+interface MapNode extends GraphNode {
   id: string;
-  name: string;
+  label: string;
+  /**
+   * Latitude and longitude of the node.
+   */
   lat: number;
   lon: number;
+  /**
+   * IDs of the neighboring nodes.
+   */
   neighborsIds: string[];
 }
 
-export const bergenNodes: BergenNode[] = [
-  // Existing nodes
+export const mapNodes: MapNode[] = [
   {
     id: "1",
-    name: "Årstad",
+    label: "Årstad",
     lat: 60.374,
     lon: 5.345,
     neighborsIds: ["2", "4"],
   },
   {
     id: "2",
-    name: "Bergenhus",
+    label: "Bergenhus",
     lat: 60.397,
     lon: 5.324,
     neighborsIds: ["1", "4"],
   },
-  { id: "3", name: "Fana", lat: 60.296, lon: 5.331, neighborsIds: ["5"] },
+  { id: "3", label: "Fana", lat: 60.296, lon: 5.331, neighborsIds: ["5"] },
   {
     id: "4",
-    name: "Laksevåg",
+    label: "Laksevåg",
     lat: 60.381,
     lon: 5.224,
     neighborsIds: ["1", "2"],
   },
-  { id: "5", name: "Ytrebygda", lat: 60.305, lon: 5.231, neighborsIds: ["3"] },
+  { id: "5", label: "Ytrebygda", lat: 60.305, lon: 5.231, neighborsIds: ["3"] },
   {
     id: "6",
-    name: "Hjellestad",
+    label: "Hjellestad",
     lat: 60.25654727937068,
     lon: 5.249927798102046,
     neighborsIds: ["5"],
   },
   {
     id: "7",
-    name: "Milde",
+    label: "Milde",
     lat: 60.25293306584283,
     lon: 5.266952394444028,
     neighborsIds: ["6"],
   },
   {
     id: "8",
-    name: "Alvøen",
+    label: "Alvøen",
     lat: 60.35971264660863,
     lon: 5.195197641250439,
     neighborsIds: ["5"],
   },
   {
     id: "9",
-    name: "Hetlevik",
+    label: "Hetlevik",
     lat: 60.35424353096282,
     lon: 5.223907726729444,
     neighborsIds: ["8"],
