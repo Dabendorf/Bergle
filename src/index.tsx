96587f4426a4bb720e64cf76d900f2ea90f8199e
AalyTokombaev <alexiver96@hotmail.com>
2024-01-25 15:24:42 +0100

    updated react-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import "./i18n";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
 
 const root = document.getElementById("root");
 
 if (root !== null) {
   // I'm not a fan
-  // TODO: Figure out how to use createRoot propperly
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  ReactDOM.createRoot(root).render(
+  createRoot(root).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
