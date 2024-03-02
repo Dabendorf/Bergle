af2a1653a573a9edebc815f05aae810fc573a05a
Lukas Schramm <lukas@dorpapst.de>
2024-03-02 11:22:21 +0100

    Linter once again fucked me

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "react-app",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint", "prettier"],
-  rules: {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: false,
-        endOfLine: "auto",
-      },
-    ],
-    quotes: ["error", "double"],
-  },
-};
+	env: {
+	  browser: true,
+	  es2021: true,
+	  node: true,
+	},
+	extends: [
+	  "react-app",
+	  "eslint:recommended",
+	  "plugin:react/recommended",
+	  "plugin:@typescript-eslint/recommended",
+	  "prettier",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+	  ecmaFeatures: {
+		jsx: true,
+	  },
+	  ecmaVersion: "latest",
+	  sourceType: "module",
+	},
+	plugins: ["react", "@typescript-eslint", "prettier"],
+	rules: {
+	  "react-hooks/rules-of-hooks": "error",
+	  "react-hooks/exhaustive-deps": "error",
+	},
+  };
