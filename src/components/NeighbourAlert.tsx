85e47b4b11ca6e1fa577cfbd0fcf00b8c93c63f0
EirikBergesen <eirikbr@yahoo.com>
2025-01-12 15:40:30 +0100

    Nabovarsel

---
+++ src/components/NeighbourAlert.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSharedGameState } from '../shared/useGame';
+import { countries } from '../domain/countries';
+
+const NeighbourAlert = () => {
+    const {
+      state: { country, guesses },
+    } = useSharedGameState();
+ const neighboursCountries = countries.filter( it => country.neighbours.includes(it.code) ).map(it => it.name.toLowerCase())
+ const lastguess  = guesses[guesses.length - 1].name.toLowerCase()
+  return (
+    <b>
+        {neighboursCountries.includes(lastguess) ? "Nå jetta du naboområdet, tjommi." : ""}
+    </b>
+  )
+}
+
+export default NeighbourAlert
